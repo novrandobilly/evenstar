48fef39f4467a962bdc8ca67760bacd7cb6be343
billy <novrandobilly@gmail.com>
2026-08-23 15:03:56 +0700

    Add ScrollToTop component to reset scroll position on route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { SessionProvider } from './context/SessionContext';
 import { ModalProvider } from './context/modal';
 import { AppRoutes } from './routes';
+import ScrollToTop from './components/ScrollToTop';
 
 export function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <ModalProvider>
         <SessionProvider>
           <AppRoutes />
